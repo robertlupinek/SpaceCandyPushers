c4be6caf469959d0c3b359821af9b76b2e2373a8
Robert Lupinek <darthlupi@gmail.com>
2018-12-16 16:56:33 -0500

    Adding slider blocks.  Forgot to add them before committing!

---
+++ Assets/02_Scripts/BlockScripts/SliderBlock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderBlock : MonoBehaviour {
+   
+    //Initial horizontal direction to move ( negative for reverse direction )
+    public float hDir = 1f;
+    //Initial horizontal direction to move ( negative for reverse direction )
+    public float vDir = 0f;
+    public float slideTimerMax = 2f;
+    //Time for changing direction
+    private float slideTimer = 2f;
+    public float hSpeed = 1f;
+    public float vSpeed = 0f;
+
+    private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        slideTimer = slideTimerMax;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rigidBody.velocity = new Vector2(hSpeed * hDir , vSpeed * vDir);
+
+        slideTimer -= Time.deltaTime;
+
+        if ( slideTimer <= 0)
+        {
+            vDir = vDir * -1;
+            hDir = hDir * -1;
+            slideTimer = slideTimerMax;
+        }
+
+    }
+}
